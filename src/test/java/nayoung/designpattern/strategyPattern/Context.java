b895165a39aaf50ea51aa091cc4a23b5cae2e154
nayoung <evelyn82ny@gmail.com>
2022-02-22 06:06:47 +0900

    feat: strategy-pattern using parameter

--- src/test/java/nayoung/designpattern/strategyPattern/Context.java
+++ src/test/java/nayoung/designpattern/strategyPattern/Context.java
@@ -1,18 +1,11 @@
 package nayoung.designpattern.strategyPattern;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class Context {
 
-    private Strategy strategy;
-
-    public Context(Strategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public void execute() {
+    public void execute(Strategy strategy) {
         long startTime = System.currentTimeMillis();
         strategy.call();
         long endTime = System.currentTimeMillis();
--- src/test/java/nayoung/designpattern/strategyPattern/StrategyPatternTest.java
+++ src/test/java/nayoung/designpattern/strategyPattern/StrategyPatternTest.java
@@ -8,10 +8,19 @@ public class StrategyPatternTest {
 
     @Test
     void strategy() {
+        /*
         Context context1 = new Context(() -> log.info("order logic"));
         context1.execute();
 
         Context context2 = new Context(() -> log.info("refund logic"));
         context2.execute();
+         */
+    }
+
+    @Test
+    void strategy_parameter() {
+        Context context = new Context();
+        context.execute(new StrategyOrder());
+        context.execute(new StrategyRefund());
     }
 }
