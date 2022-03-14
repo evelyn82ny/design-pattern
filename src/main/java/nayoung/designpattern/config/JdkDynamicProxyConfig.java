3f94842723ead39662c9e17640da0f2b71a3eee3
nayoung <evelyn82ny@gmail.com>
2022-03-15 02:05:16 +0900

    chore: set the filter

--- src/main/java/nayoung/designpattern/config/JdkDynamicProxyConfig.java
+++ src/main/java/nayoung/designpattern/config/JdkDynamicProxyConfig.java
@@ -11,11 +11,13 @@ import java.lang.reflect.Proxy;
 @Configuration
 public class JdkDynamicProxyConfig {
 
+    public static final String[] PATTERNS = {"*NoLog*"};
+
     @Bean
     public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
         OrderControllerV1 orderController = new OrderControllerV1Impl(orderServiceV1(logTrace));
         OrderControllerV1 proxy = (OrderControllerV1) Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(),
-                new Class[]{OrderControllerV1.class}, new LogTraceHandler(orderController, logTrace));
+                new Class[]{OrderControllerV1.class}, new LogTraceHandler(orderController, logTrace, PATTERNS));
         return proxy;
     }
 
@@ -23,7 +25,7 @@ public class JdkDynamicProxyConfig {
     public OrderServiceV1 orderServiceV1(LogTrace logTrace) {
         OrderServiceV1 orderService = new OrderServiceV1Impl(itemRepositoryV1(logTrace));
         OrderServiceV1 proxy = (OrderServiceV1) Proxy.newProxyInstance(OrderServiceV1.class.getClassLoader(),
-                new Class[]{OrderServiceV1.class}, new LogTraceHandler(orderService, logTrace));
+                new Class[]{OrderServiceV1.class}, new LogTraceHandler(orderService, logTrace, PATTERNS));
         return proxy;
     }
 
@@ -31,7 +33,7 @@ public class JdkDynamicProxyConfig {
     public ItemRepositoryV1 itemRepositoryV1(LogTrace logTrace) {
         ItemRepositoryV1 itemRepository = new ItemRepositoryV1Impl();
         ItemRepositoryV1 proxy = (ItemRepositoryV1) Proxy.newProxyInstance(ItemRepositoryV1.class.getClassLoader(),
-                new Class[]{ItemRepositoryV1.class}, new LogTraceHandler(itemRepository, logTrace));
+                new Class[]{ItemRepositoryV1.class}, new LogTraceHandler(itemRepository, logTrace, PATTERNS));
         return proxy;
     }
 }
--- src/main/java/nayoung/designpattern/trace/LogTraceHandler.java
+++ src/main/java/nayoung/designpattern/trace/LogTraceHandler.java
@@ -1,20 +1,29 @@
 package nayoung.designpattern.trace;
 
+import org.springframework.util.PatternMatchUtils;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class LogTraceHandler implements InvocationHandler {
     private final Object target;
     private final LogTrace logTrace;
+    private final String[] patterns;
 
-    public LogTraceHandler(Object target, LogTrace logTrace) {
+    public LogTraceHandler(Object target, LogTrace logTrace, String... patterns) {
         this.target = target;
         this.logTrace = logTrace;
+        this.patterns = patterns;
     }
-
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        String methodName = method.getName();
+        if(PatternMatchUtils.simpleMatch(patterns, methodName)) {
+            return method.invoke(target, args);
+        }
+
         TraceStatus status = null;
         try {
             String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
