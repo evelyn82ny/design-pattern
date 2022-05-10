9fe31696df4e7cc6a6eab3ef1670c5966416279c
nayoung <evelyn82ny@gmail.com>
2022-05-10 17:06:16 +0900

    test: setProxyTargetClass(true) (ProxyFactory)

--- src/test/java/nayoung/designpattern/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/nayoung/designpattern/proxyfactory/ProxyFactoryTest.java
@@ -48,4 +48,21 @@ public class ProxyFactoryTest {
         Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
         Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
     }
+
+    @Test
+    void proxyTargetClass() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("target class = {}", target.getClass());
+        log.info("proxy class = {}", proxy.getClass());
+        proxy.save();
+
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
 }
