0ac54c4e2ee1eb6a79004e271cd52b807cbb7d51
nayoung <evelyn82ny@gmail.com>
2022-05-12 19:12:38 +0900

    test: Multi Advisor

---
+++ src/test/java/nayoung/designpattern/proxyfactory/MultiAdvisorTest.java
@@ -0,0 +1,52 @@
+package nayoung.designpattern.proxyfactory;
+
+import lombok.extern.slf4j.Slf4j;
+import nayoung.designpattern.JDKDynamicProxy.service.ServiceImpl;
+import nayoung.designpattern.JDKDynamicProxy.service.ServiceInterface;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+public class MultiAdvisorTest {
+
+    @Test
+    void multiAdvisorTest() {
+        // client -> proxy2(advisor2) -> proxy1(advisor1) -> target
+
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory1.getProxy();
+
+        ProxyFactory proxyFactory2 = new ProxyFactory(proxy1);
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        proxyFactory2.addAdvisor(advisor2);
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy();
+
+        proxy2.save();
+    }
+
+    @Slf4j
+    static class Advice1 implements MethodInterceptor {
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice1 호출");
+            return invocation.proceed();
+        }
+    }
+
+    @Slf4j
+    static class Advice2 implements MethodInterceptor {
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice2 호출");
+            return invocation.proceed();
+        }
+    }
+}
