aaaa99fa1397c4e6ae1d4484ab0f55d3cfd9a29e
nayoung <evelyn82ny@gmail.com>
2022-05-12 16:11:31 +0900

    test: implement Pointcut

--- src/test/java/nayoung/designpattern/proxyfactory/AdvisorTest.java
+++ src/test/java/nayoung/designpattern/proxyfactory/AdvisorTest.java
@@ -5,9 +5,13 @@ import nayoung.designpattern.JDKDynamicProxy.service.ServiceImpl;
 import nayoung.designpattern.JDKDynamicProxy.service.ServiceInterface;
 import nayoung.designpattern.advice.TimeAdvice;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
 
 @Slf4j
 public class AdvisorTest {
@@ -22,5 +26,52 @@ public class AdvisorTest {
         ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
         proxy.save();
         proxy.find();
+    }
+
+    @Test
+    void addPointcutTest() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        proxy.find();
+        proxy.save();
+    }
+
+    static class MyPointcut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher {
+        private String matchName = "save";
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("pointcut 호출 method = {} target class = {}", method.getName(), targetClass);
+            log.info("pointcut 결과 result = {}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
     }
 }
