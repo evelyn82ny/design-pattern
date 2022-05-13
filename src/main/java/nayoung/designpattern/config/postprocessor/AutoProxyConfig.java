fa6dc11c3018009eab646f7ad38853448f2e4528
nayoung <evelyn82ny@gmail.com>
2022-05-13 19:44:50 +0900

    feat: use execution

--- src/main/java/nayoung/designpattern/config/postprocessor/AutoProxyConfig.java
+++ src/main/java/nayoung/designpattern/config/postprocessor/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import nayoung.designpattern.config.AppV2Config;
 import nayoung.designpattern.config.advice.LogTraceAdvice;
 import nayoung.designpattern.trace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,26 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-    @Bean
+    //@Bean
     public Advisor advisor(LogTrace logTrace) {
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedName("*PrintLog");
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    //@Bean
+    public Advisor setExecutionAdvisor(LogTrace logTrace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* nayoung.designpattern.app..*(..))");
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    //@Bean
+    public Advisor setExecutionAdvisor2(LogTrace logTrace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* nayoung.designpattern.app..*(..)) && !execution(* nayoung.designpattern.app..*NoLog(..))");
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
         return new DefaultPointcutAdvisor(pointcut, advice);
     }
